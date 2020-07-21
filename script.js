5ad76bbec5f0ba451b6d9e28dc5243dcc0f8b6af
Rohit Sangha <1RohitSangha@gmail.com>
2020-07-21 00:00:21 -0600

    misc

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  wrapAround: true,
+  autoPlay: true
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
-  // options
+  
 });
 
+document.querySelector(".message").addEventListener("click", (event) => {event.preventDefault()});
